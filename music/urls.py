bfd5a6e4809aecb9325bc11bc7f6e503509e0eef
unknown <v.dhanendra@gmail.com>
2019-06-24 00:14:16 +0530

    Added models and details for database

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
+	#/music/
     re_path(r'^$', views.index),
+
+    #/music/17/
+    re_path(r'^(?P<album_id>[0-9]+)/$',views.detail)
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
 
 def index(request):
-	return HttpResponse("<h1>this is the music app homepage </h1>")
+	all_albums = Album.objects.all()	
+	context = {
+		'all_albums' : all_albums,
+	}
+	return render(request, 'music/index.html', {'all_albums':all_albums})
+
+
+def detail(request,album_id):
+	try:
+		album = Album.objects.filter(id=album_id).get()
+	except:	
+		raise Http404("Album doesn't exist")
+	return render(request, 'music/details.html', {'album':album})
